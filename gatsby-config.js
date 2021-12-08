6d15227ea61c9f35be0560ca7c8b56c815a0855e
Cláudio Martins <44114245+Claudio-Martins@users.noreply.github.com>
2021-12-08 17:25:50 +0000

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,47 +15,4 @@ module.exports = {
     siteLanguage: `pt`,
     siteImage: `/banner.jpg`,
     author: `@camaf.eu`,
-  },
-  plugins: [
-    //{
-      //resolve: `@lekoarts/gatsby-theme-cara`,
-      // See the theme's README for all available options
-      //options: {},
-    //},
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `Cara - @lekoarts/gatsby-theme-cara`,
-        short_name: `Cara`,
-        description: `Colorido, animado, pagina-unica, portfolio com Parallax e animacoes`,
-        start_url: `/`,
-        background_color: `#141821`,
-        // This will impact how browsers show your PWA/website
-        // https://css-tricks.com/meta-theme-color-and-trickery/
-        // theme_color: `#f6ad55`,
-        display: `standalone`,
-        icons: [
-          {
-            src: `/android-chrome-192x192.png`,
-            sizes: `192x192`,
-            type: `image/png`,
-          },
-          {
-            src: `/android-chrome-512x512.png`,
-            sizes: `512x512`,
-            type: `image/png`,
-          },
-        ],
-      },
-    },
-    `gatsby-plugin-gatsby-cloud`,
-    shouldAnalyseBundle && {
-      resolve: `gatsby-plugin-webpack-bundle-analyser-v2`,
-      options: {
-        analyzerMode: `static`,
-        reportFilename: `_bundle.html`,
-        openAnalyzer: false,
-      },
-    },
-  ].filter(Boolean),
-}
+  }
